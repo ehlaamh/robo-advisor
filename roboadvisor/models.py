0b66e20ce62f07516db80f53a9109910ad4ee425
Aditisp <87130640+Aditisp@users.noreply.github.com>
2021-08-23 19:43:29 +0530

    Delete roboadvisor directory

--- roboadvisor/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Userreg(models.Model):
-    name=models.CharField(max_length=50)
-    email=models.CharField(max_length=50)
-    password=models.CharField(max_length=50)
-    class Meta:
-        db_table="user"
